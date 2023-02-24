5bbd3fba377cf2bc94cfe062d957ade77c2a6cb8
hudy9x <hudy9x@gmail.com>
2023-02-24 14:10:41 +0700

    chore: merge old homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
 import type { AppType } from 'next/app';
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
 import { trpc } from '../utils/trpc';
 // styles
 import '../styles/sidebar.css'
@@ -7,7 +9,16 @@ import '../components/Confirmbox/style.css'
 import '../components/Controls/InputControl/style.css'
 import '../components/Message/style.css'
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppType & {
+  Component: NextPageWithLayout
+}
+
+const MyApp: AppPropsWithLayout = ({ Component, pageProps }) => {
+  const getLayout = Component.getLayout ?? ((page) => page)
+  return getLayout(<Component {...pageProps} />)
 };
 export default trpc.withTRPC(MyApp);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./containers/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/containers/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: "class",
   mode: "jit",
